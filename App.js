3eec557ad63b7a2bb5cfe4d86bc5d1d928799034
Douglas6969 <133924667+Douglas6969@users.noreply.github.com>
2023-11-16 19:48:17 -0300

    Teste

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Text>Oi</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
