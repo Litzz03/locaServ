bef1c68b15cea2782ca4e164b0c112010fa17cee
Espaniiol <guilhermeespaniol@gmail.com>
2023-11-16 20:00:27 -0300

    Teste2

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text>Oig</Text>
       <Text>Teste</Text>
+      <Text>Teste</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
