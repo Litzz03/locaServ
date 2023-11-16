507015a8b524c7b06899f3afca890392b4d25cb7
Espaniiol <guilhermeespaniol@gmail.com>
2023-11-16 19:55:45 -0300

    testeGit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Oig</Text>
+      <Text>Teste</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
