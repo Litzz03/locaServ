fb116d46dd14c46d2117d620fd4b1f46ba786037
Douglas6969 <133924667+Douglas6969@users.noreply.github.com>
2023-11-16 21:59:40 -0300

    co

--- App.js
+++ App.js
@@ -2,13 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import InputGeneric from './src/components/InputGeneric';
 import Header from './src/components/Header';
-<<<<<<< HEAD
 import ButonsScreen from './src/components/ButonScreen';
-
-=======
-import Splash from './src/screens/Splash';
 import ButonsScreen from './src/components/ButonScreen';
->>>>>>> 75928d1c4b0343ba1c97d09ffdc8aa2525545a52
 
 
 export default function App() {
